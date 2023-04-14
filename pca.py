1d0125b95bf4fc02a6bb19d654620ea3cf257c3b
Vratin Srivastava <114123331+vratins@users.noreply.github.com>
2023-04-14 15:26:24 -0400

    PCA edits
    Plot+Code Changes

--- pca.py
+++ pca.py
@@ -1,7 +1,7 @@
 import numpy as np
-from PIL import Image
 import matplotlib.pyplot as plt
 import pandas as pd
+
 
 def pca(X, D):
     """
@@ -12,10 +12,10 @@ def pca(X, D):
     Output:
         X - A compressed (1024, 1024) numpy array
     """
+    X = (X - np.mean(X, axis=0))/np.std(X, axis=0)
     u, e, v = np.linalg.svd(np.dot(X.T, X))
     eigen = v.T[:, 0:D]
     final = np.dot(X, eigen)
-    #return np.dot(final, eigen.T)
     return final
 
 
@@ -24,19 +24,22 @@ def sklearn_pca(X, D):
     Your PCA implementation should be equivalent to this function.
     Do not use this function in your implementation!
     """
+    X = (X - np.mean(X, axis=0))/np.std(X, axis=0)
     from sklearn.decomposition import PCA
     p = PCA(n_components=D, svd_solver='full')
     trans_pca = p.fit_transform(X)
-    X = p.inverse_transform(trans_pca)
-    return X
+    return trans_pca
 
 
 if __name__ == '__main__':
-    X = pd.read_csv("Data/BAL/normdataBAL0715.txt", sep = '\t', skiprows= range(1001, 45015), usecols = [2,154] )
-    print(X)
+    X = pd.read_csv("Data/BAL/normdataBAL0715.txt", sep = '\t', usecols=range(2,154))
     D = 2
-    pca(X, D)
-    sklearn_pca(X, D)
+    pca_final=pca(X, D)
+    pca_sklearn=sklearn_pca(X, D)
+
+    plt.scatter(pca_final[:, 0], pca_final[:, 1])
+    plt.title('PCA')
+    plt.show()
 
 
 
