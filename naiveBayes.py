f89ceddc4ac48c836c2eff2909e103070a7ca786
sinhanushka12 <anushka3@andrew.cmu.edu>
2023-04-26 18:03:37 -0400

    updated

---
+++ naiveBayes.py
@@ -0,0 +1,11 @@
+from sklearn.datasets import make_classification
+
+X, y = make_classification(
+    numFeatures= ,
+    numClasses= 3,
+    numSamples= 800,
+    numInformative=2,
+    randomState=1,
+    numClustersPerClass=1,
+)
+
